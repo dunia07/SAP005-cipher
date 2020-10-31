c36cc1f36f44c511e72eaefc5a6fc6649f6a3f66
Dunia <dunia07@hotmail.com>
2020-10-30 23:47:15 -0300

    Desenvolvimento dos arquivos index.js e cipher.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
+//.addEventListener("click", function(event) = onclick="" no HTML
+
 import cipher from './cipher.js';
 
-console.log(cipher);
+function encodeCipher(){
+
+    let message = document.getElementById("message").value;
+    let offset = document.getElementById("offset").value;
+    let result = document.getElementById("result");
+    let resultCipher = cipher.encode(offset, message);
+
+    result.value=resultCipher;
+}
+
+function decodeCipher(){
+
+    let message = document.getElementById("message").value;
+    let offset = document.getElementById("offset").value;
+    let result = document.getElementById("result");
+    let resultCipher = cipher.decode(offset, message);
+
+    result.innerHTML=resultCipher;
+}
+
+
+document.getElementById("encodeBtn").addEventListener("click", function(event){
+    event.preventDefault();
+    encodeCipher();
+});
+document.getElementById("decodeBtn").addEventListener("click", function(event){
+    event.preventDefault();
+    decodeCipher();
+});
