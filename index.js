809dc1bff1dfb596ae19948d6796977902261e15
Dunia <dunia07@hotmail.com>
2020-11-06 14:04:03 -0300

    Projeto finalizado

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 //.addEventListener("click", function(event) = onclick="" no HTML
+//.toUpperCase = transf. lower to upper
+//Number() = transf. String to Number
 
 import cipher from './cipher.js';
 
 function encodeCipher(){
 
-    let message = document.getElementById("message").value;
+    let message = document.getElementById("message").value.toUpperCase();
     let offset = Number(document.getElementById("offset").value);
     let result = document.getElementById("result");
     let resultCipher = cipher.encode(offset, message);
@@ -14,7 +16,7 @@ function encodeCipher(){
 
 function decodeCipher(){
 
-    let message = document.getElementById("message").value;
+    let message = document.getElementById("message").value.toUpperCase();
     let offset = Number(document.getElementById("offset").value);
     let result = document.getElementById("result");
     let resultCipher = cipher.decode(offset, message);
