eea040525996f3f290fc5ce784b2acf6a7cfa99a
Dunia <dunia07@hotmail.com>
2020-11-06 14:04:03 -0300

    Projeto finalizado

--- src/cipher.js
+++ src/cipher.js
@@ -28,7 +28,6 @@ const cipher = {
 
       }
     }
-    //document.getElementById("result").innerHTML=finalResult;
     return finalResult;
 
   },
@@ -48,7 +47,6 @@ const cipher = {
 
       }
     }
-    //document.getElementById("result").innerHTML=finalResult;
     return finalResult;
   }
   
